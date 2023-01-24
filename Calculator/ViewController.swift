3ddea8473fc824fad6bd0b844afadd27520fb265
Samet Balcı <sametbalci83@gmail.com>
2023-01-24 20:45:18 +0300

    chore: view control

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var calculateLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func pressPlus(_ sender: Any) {
+        if let firstNumber = Int(firstNumber.text!) {
+            if let secondNumber = Int(secondNumber.text!) {
+                let result = firstNumber + secondNumber
+                calculateLabel.text = String(result)
+            }
+        }
+    }
+    @IBAction func pressEksi(_ sender: Any) {
+        if let firstNumber = Int(firstNumber.text!) {
+            if let secondNumber = Int(secondNumber.text!) {
+                let result = firstNumber - secondNumber
+                calculateLabel.text = String(result)
+            }
+        }
+    }
+    @IBAction func pressX(_ sender: Any) {
+        if let firstNumber = Int(firstNumber.text!) {
+            if let secondNumber = Int(secondNumber.text!) {
+                let result = firstNumber * secondNumber
+                calculateLabel.text = String(result)
+            }
+        }
+    }
+    @IBAction func pressBolme(_ sender: Any) {
+        if let firstNumber = Int(firstNumber.text!) {
+            if let secondNumber = Int(secondNumber.text!) {
+                let result = firstNumber / secondNumber
+                calculateLabel.text = String(result)
+            }
+        }
+    }
 }
 
